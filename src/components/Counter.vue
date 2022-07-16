e4924ada088466e0e9ae32216117701bc7fc6c22
gadd-gj <gaddielgomez15@gmail.com>
2022-07-15 19:24:08 -0500

    properties en vuejs

---
+++ src/components/Counter.vue
@@ -0,0 +1,77 @@
+<template>
+    <h2> {{ customTitle }} </h2>
+    <p> {{ counter }}  <sup>2</sup> = {{ squareCounter }} </p>
+
+    <div>
+        <button @click="increase">+1</button>
+        <button @click="decrease">-1</button>
+    </div>
+</template>
+
+
+<script>
+
+export default ({
+
+    props: {
+        title: String,
+        start: {
+            type: Number,
+            default: 10,
+            validator(value) {
+                return value >= 0
+            } 
+
+        }
+    },
+
+    data() {
+        return {
+            counter: this.start
+        }
+    },
+    methods: {
+        getSquare() {
+            console.log('getSquare')
+            return this.counter * this.counter
+        },
+        increase() {
+            this.counter++ 
+        },
+        decrease() {
+            this.counter--
+        }
+    },
+    computed: {
+        squareCounter() {
+            console.log('squareCounter')
+            return this.counter * this.counter
+        },
+        customTitle() {
+            return this.title || 'Counter'
+        }
+    }
+
+})
+</script>
+
+
+
+<style>
+
+button {
+    background-color: #64BB87;
+    border-radius: 5px;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+}
+
+button:hover {
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out;
+}
+
+</style>
